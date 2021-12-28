a60c7b97a57bbb7e648e3620efe8be43be56d14f
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-12-28 18:41:06 +0100

    fixed quiet flags

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q http://cs231n.stanford.edu/tiny-imagenet-200.zip
-unzip tiny-imagenet-200.zip
+wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+unzip -q tiny-imagenet-200.zip
